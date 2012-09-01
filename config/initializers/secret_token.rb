b568a6acc30dd5ae622bc1991b3ac4a6d4bbdb1b
Ruogu Luo <russell.lrg@gmail.com>
2012-09-01 17:56:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expless::Application.config.secret_token = '1b7c77b38ac29afb38be2a238812649f4a7b3b73d3158fd838708104aa8b92248bbaff698d5075e18d044705febbf2e526172da86396265dba1be77601f23606'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Expless::Application.config.session_store :cookie_store, key: '_expless_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Expless::Application.config.session_store :active_record_store
